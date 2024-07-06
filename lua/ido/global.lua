d902fc1672f313e97b3245a7084982aacf6e3334
haoliang <haoliang0.1.2@gmail.com>
2024-07-06 20:18:36 +0800

    sync with upstream

--- lua/ido/global.lua
+++ lua/ido/global.lua
@@ -1,5 +1,6 @@
 local M = {}
 
+local ascii = require("infra.ascii")
 local augroups = require("infra.augroups")
 local buflines = require("infra.buflines")
 local feedkeys = require("infra.feedkeys")
@@ -11,12 +12,30 @@ local vsel = require("infra.vsel")
 local wincursor = require("infra.wincursor")
 
 local beckon_select = require("beckon.select")
+local ropes = require("string.buffer")
 
 local uv = vim.uv
 
 local anchor_ns = ni.create_namespace("ido:global:anchors")
 
-local xmarks = {}
+local resolve_pattern
+do
+  local rope = ropes.new(64)
+
+  ---transform:
+  ---* to fixed string
+  ---* add word boundaries
+  ---@param keyword string
+  function resolve_pattern(keyword)
+    keyword = vim.fn.escape(keyword, [[.$*~\]])
+    if ascii.is_letter(string.sub(keyword, 1, 1)) then rope:put([[\<]]) end
+    rope:put(keyword)
+    if ascii.is_letter(string.sub(keyword, -1, -1)) then rope:put([[\>]]) end
+    return rope:get()
+  end
+end
+
+local anchors = {}
 do
   ---@class ido.XmarkPos
   ---@field start_lnum integer
@@ -27,7 +46,7 @@ do
   ---@param bufnr integer
   ---@param xmid integer
   ---@return ido.XmarkPos?
-  function xmarks.pos(bufnr, xmid)
+  function anchors.pos(bufnr, xmid)
     local xm = ni.buf_get_extmark_by_id(bufnr, anchor_ns, xmid, { details = true })
     if xm[3].invalid then return end
     return { start_lnum = xm[1], start_col = xm[2], stop_lnum = xm[3].end_row, stop_col = xm[3].end_col }
@@ -36,10 +55,10 @@ do
   ---@param bufnr integer
   ---@param id_or_pos integer|ido.XmarkPos
   ---@return string[]|nil
-  function xmarks.text(bufnr, id_or_pos)
+  function anchors.text(bufnr, id_or_pos)
     local pos
     if type(id_or_pos) == "number" then
-      pos = xmarks.pos(bufnr, id_or_pos)
+      pos = anchors.pos(bufnr, id_or_pos)
       if pos == nil then return end
     else
       pos = id_or_pos
@@ -47,6 +66,23 @@ do
 
     return ni.buf_get_text(bufnr, pos.start_lnum, pos.start_col, pos.stop_lnum, pos.stop_col, {})
   end
+
+  ---@param bufnr integer
+  ---@param origin ido.Origin
+  ---@param group string
+  function anchors.set(bufnr, origin, group)
+    --stylua: ignore start
+    return ni.buf_set_extmark(bufnr, anchor_ns, origin.lnum, origin.start_col, {
+      end_row = origin.lnum, end_col = origin.stop_col,
+      hl_group = group, hl_mode = "replace",
+      right_gravity = false, end_right_gravity = true,
+    })
+    --stylua: ignore end
+  end
+
+  ---@param bufnr integer
+  ---@param xmid integer
+  function anchors.del(bufnr, xmid) ni.buf_del_extmark(bufnr, anchor_ns, xmid) end
 end
 
 local Debounce
@@ -77,180 +113,213 @@ do
   end
 end
 
+local Session
+do
+  ---@class ido.Origin
+  ---@field lnum integer
+  ---@field start_col integer
+  ---@field stop_col integer
+
+  ---truth_{idx,xmid} -> truth of source; anchor
+  ---
+  ---@class ido.Session
+  ---
+  ---@field winid integer
+  ---@field bufnr integer
+  ---
+  ---@field keyword string
+  ---@field origins ido.Origin[]
+  ---@field truth_idx integer
+  ---
+  ---@field xmids integer[]
+  ---@field truth_xmid integer @==xmids[truth_idx]
+  ---@field aug infra.BufAugroup
+  ---@field debounce ido.Debounce
+  local Impl = {}
+  Impl.__index = Impl
+
+  function Impl:activate() --
+    do --place anchors
+      for i = 1, #self.origins do
+        local origin = self.origins[i]
+        --todo: dedicated hlgroups for ido
+        local group = i == self.truth_idx and "Todo" or "Search"
+        self.xmids[i] = anchors.set(self.bufnr, origin, group)
+      end
+      self.truth_xmid = self.xmids[self.truth_idx]
+    end
+
+    self.aug = augroups.BufAugroup(self.bufnr, true, string.format("ido://%d", self.bufnr))
+    self.debounce = Debounce(125)
+
+    do --sync mechanism
+      ---known facts:
+      ---* buf_set_text wont trigger TextChanged/I in insert/normal mode
+      ---* undo also triggers TextChanged and buf_set_text here creates undo blocks, this leads infinite undo
+      ---
+      ---design choices
+      ---* only sync changes from truth_xm
+      ---* allow change other xms, but no syncing
+      ---
+      ---workaround for undo/redo
+      ---* compare last_text and truth_text to avoid replicate triggering
+
+      local origin_text = anchors.text(self.bufnr, self.truth_xmid)
+
+      --workaround of undo/redo
+      local last_text = {} ---@type string[]
+
+      local function on_change()
+        local truth_text = anchors.text(self.bufnr, self.truth_xmid)
+        if truth_text == nil then jelly.info("anchor#0 has gone") end
+        if truth_text == nil then return true end
+
+        if truth_text == origin_text then return jelly.debug("no changes") end
+        if itertools.equals(truth_text, last_text) then return jelly.debug("no changes") end
+
+        self.debounce:start_soon(function()
+          last_text = truth_text
+
+          for i = 1, #self.xmids do
+            if i == self.truth_idx then goto continue end
+            local pos = anchors.pos(self.bufnr, self.xmids[i])
+            if pos == nil then goto continue end
+            ni.buf_set_text(self.bufnr, pos.start_lnum, pos.start_col, pos.stop_lnum, pos.stop_col, truth_text)
+            ::continue::
+          end
+        end)
+      end
+      self.aug:repeats({ "TextChanged", "TextChangedI" }, { callback = on_change })
+    end
+
+    do --initial cursor
+      local pos = assert(anchors.pos(self.bufnr, self.truth_xmid))
+      assert(pos.stop_col > 0)
+      wincursor.go(self.winid, pos.stop_lnum, pos.stop_col - 1)
+      feedkeys("a", "n")
+    end
+  end
+
+  ---@return string
+  function Impl:title()
+    local pos = anchors.pos(self.bufnr, self.truth_xmid)
+    local lnum = pos and pos.start_lnum or "n/a"
+    return string.format("buf#%d:%s '%s'", self.bufnr, lnum, self.keyword)
+  end
+
+  function Impl:deactivate()
+    self.aug:unlink()
+    self.debounce:close()
+    for _, xmid in ipairs(self.xmids) do
+      anchors.del(self.bufnr, xmid)
+    end
+  end
+
+  ---@param winid integer
+  ---@param keyword string
+  ---@return ido.Session?
+  function Session(winid, keyword)
+    local bufnr = ni.win_get_buf(winid)
+    local cursor = wincursor.last_position()
+
+    local origins = {} ---@type ido.Origin[]
+    do
+      local regex = assert(VimRegex(resolve_pattern(keyword)))
+      for lnum = 0, buflines.high(bufnr) do
+        for start_col, stop_col in regex:iter_line(bufnr, lnum) do
+          table.insert(origins, { lnum = lnum, start_col = start_col, stop_col = stop_col })
+        end
+      end
+      if #origins < 2 then return jelly.warn("no other matches") end
+    end
+
+    local truth_idx
+    do
+      local min_dis
+      for i = 1, #origins do
+        local origin = origins[i]
+        if origin.lnum ~= cursor.lnum then goto continue end
+        local dis = math.min(math.abs(origin.start_col - cursor.col), math.abs(origin.stop_col - cursor.col))
+        if min_dis == nil or dis < min_dis then
+          min_dis = dis
+          truth_idx = i
+        end
+        ::continue::
+      end
+      if truth_idx == nil then truth_idx = 1 end
+    end
+
+    --stylua: ignore start
+    return setmetatable({
+      winid = winid, bufnr = bufnr,
+      keyword = keyword, origins = origins, truth_idx = truth_idx,
+      xmids = {}, truth_xmid = nil,
+    }, Impl)
+    --stylua: ignore end
+  end
+end
+
 local sessions = {}
 do
-  ---@class ido.Session
-  ---@field title string @used for M.deactive.select_one_to_deactive
-  ---@field deactive fun()
-
   ---{bufnr:Session}
   ---@type {[integer]: ido.Session}
   sessions.kv = {}
 
   function sessions:is_active(bufnr) return self.kv[bufnr] ~= nil end
 
-  function sessions:activate(bufnr, ses)
-    assert(self.kv[bufnr] == nil, "this buf has already activated a session")
-    self.kv[bufnr] = ses
+  ---@param ses ido.Session
+  function sessions:activate(ses)
+    assert(self.kv[ses.bufnr] == nil, "this buf has already activated a session")
+    self.kv[ses.bufnr] = ses
+    ses:activate()
   end
 
   function sessions:deactivate(bufnr)
     local ses = self.kv[bufnr]
     if ses == nil then return end
     self.kv[bufnr] = nil
-    ses.deactive()
+    ses:deactivate()
   end
 end
-
----@class ido.Origin
----@field lnum integer
----@field start_col integer
----@field stop_col integer
 
 ---@param winid integer
 function M.activate(winid)
   winid = winid or ni.get_current_win()
-  local bufnr = ni.win_get_buf(winid)
-  local cursor = wincursor.last_position()
 
+  local bufnr = ni.win_get_buf(winid)
   sessions:deactivate(bufnr)
 
   local keyword = vsel.oneline_text(bufnr)
   if keyword == nil then return jelly.info("no selecting keyword") end
-  keyword = vim.fn.escape(keyword, [[.$*~\]])
 
-  ni.buf_clear_namespace(bufnr, anchor_ns, 0, -1)
-
-  local origins = {} ---@type ido.Origin[]
-  do
-    local regex = assert(VimRegex(string.format([[\<%s\>]], keyword)))
-    for lnum = 0, buflines.high(bufnr) do
-      for start_col, stop_col in regex:iter_line(bufnr, lnum) do
-        table.insert(origins, { lnum = lnum, start_col = start_col, stop_col = stop_col })
-      end
-    end
-    if #origins < 2 then return jelly.info("no other matches") end
-  end
-
-  local truth_idx
-  do
-    local min_dis
-    for i = 1, #origins do
-      local origin = origins[i]
-      if origin.lnum ~= cursor.lnum then goto continue end
-      local dis = math.min(math.abs(origin.start_col - cursor.col), math.abs(origin.stop_col - cursor.col))
-      if min_dis == nil or dis < min_dis then
-        min_dis = dis
-        truth_idx = i
-      end
-      ::continue::
-    end
-    if truth_idx == nil then return jelly.fatal("unreachable", "no truth_idx found: cursor=%s; origins: %s", cursor, origins) end
-  end
-
-  local xmids = {} ---@type integer[]
-  local truth_xmid
-  for i = 1, #origins do
-    local origin = origins[i]
-    --todo: dedicated hlgroups for ido
-    local group = i == truth_idx and "Todo" or "Search"
-    --stylua: ignore start
-    local xmid = ni.buf_set_extmark(bufnr, anchor_ns, origin.lnum, origin.start_col, {
-      end_row = origin.lnum, end_col = origin.stop_col,
-      hl_group = group, hl_mode = "replace",
-      right_gravity = false, end_right_gravity = true,
-    })
-    --stylua: ignore end
-    xmids[i] = xmid
-    if i == truth_idx then truth_xmid = xmid end
-  end
-
-  local aug = augroups.BufAugroup(bufnr, true, string.format("ido://%d", bufnr))
-  local debounce = Debounce(125)
-  do
-    ---known facts:
-    ---* buf_set_text wont trigger TextChanged/I in insert/normal mode
-    ---* undo also triggers TextChanged and buf_set_text here creates undo blocks, this leads infinite undo
-    ---
-    ---design choices
-    ---* only sync changes from truth_xm
-    ---* allow change other xms, but no syncing
-    ---
-    ---workaround for undo/redo
-    ---* compare last_text and truth_text to avoid replicate triggering
-
-    local origin_text = xmarks.text(bufnr, truth_xmid)
-
-    --workaround of undo/redo
-    local last_text = {} ---@type string[]
-
-    local function on_change()
-      local truth_text = xmarks.text(bufnr, truth_xmid)
-      if truth_text == nil then jelly.info("anchor#0 has gone") end
-      if truth_text == nil then return true end
-
-      if truth_text == origin_text then return jelly.debug("no changes") end
-      if itertools.equals(truth_text, last_text) then return jelly.debug("no changes") end
-
-      debounce:start_soon(function()
-        last_text = truth_text
-
-        for i = 1, #xmids do
-          if i == truth_idx then goto continue end
-          local pos = xmarks.pos(bufnr, xmids[i])
-          if pos == nil then goto continue end
-          ni.buf_set_text(bufnr, pos.start_lnum, pos.start_col, pos.stop_lnum, pos.stop_col, truth_text)
-          ::continue::
-        end
-      end)
-    end
-    aug:repeats({ "TextChanged", "TextChangedI" }, { callback = on_change })
-  end
-
-  sessions:activate(bufnr, {
-    title = function()
-      local pos = xmarks.pos(bufnr, truth_xmid)
-      local lnum = pos and pos.start_lnum or "n/a"
-      return string.format("buf#%d:%s '%s'", bufnr, lnum, keyword)
-    end,
-    deactive = function()
-      aug:unlink()
-      debounce:close()
-      ni.buf_clear_namespace(bufnr, anchor_ns, 0, -1)
-    end,
-  })
-
-  do
-    local pos = assert(xmarks.pos(bufnr, truth_xmid))
-    assert(pos.stop_col > 0)
-    wincursor.go(winid, pos.stop_lnum, pos.stop_col - 1)
-    feedkeys("a", "n")
-  end
+  local ses = Session(winid, keyword)
+  if ses == nil then return end
+  sessions:activate(ses)
 end
 
-do --M.deactive
-  local function select_one_to_deactive()
+do --M.deactivate
+  local function select_one_to_deactivate()
     local entries = {}
     local bufs = {}
     for bufnr, ses in pairs(sessions.kv) do
-      table.insert(entries, ses.title())
+      table.insert(entries, ses:title())
       table.insert(bufs, bufnr)
     end
     if #entries == 0 then return jelly.info("no active sessions") end
 
-    beckon_select(entries, { prompt = "ido deactive" }, function(_, row)
+    beckon_select(entries, { prompt = "ido deactivate" }, function(_, row)
       local nr = assert(bufs[row])
       sessions:deactivate(nr)
     end)
   end
 
-  function M.deactive(winid)
+  function M.deactivate(winid)
     winid = winid or ni.get_current_win()
     local bufnr = ni.win_get_buf(winid)
 
     if sessions:is_active(bufnr) then return sessions:deactivate(bufnr) end
 
-    select_one_to_deactive()
+    select_one_to_deactivate()
   end
 end
 
